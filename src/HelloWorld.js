e5eabad3a60d0fd76c8554d40ebb1730f7e61f6d
Andrew Jessen-Tyler <andrewtyl@gmail.com>
2019-07-31 11:00:39 -0500

    helloWorld.js work

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
-function HelloWorld() {
-  return (
-    <div className="HelloWorld">
-        <p>Hello, </p>
-    </div>
-  );
+class HelloWorld extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            world: 'Hello world!',
+            react: 'Hello React!',
+            friend: 'Hello friend!'
+        }
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>{this.state.world}</p>
+                <button onClick={function() {console.log('world button clicked')}}>World</button>
+                <button onClick={function() {console.log('react button clicked')}}>React</button>
+                <button onClick={function() {console.log('friend button clicked')}}>Friend</button>
+            </div>
+        )
+    }
 }
 
 export default HelloWorld;
